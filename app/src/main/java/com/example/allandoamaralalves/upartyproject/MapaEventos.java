4731f3a1bb39b3c68a69dc02a56740fedf9f4a42
marceladomingues <marcela.domingues@gmail.com>
2015-09-16 20:30:44 -0300

    google maps activity

--- app/src/main/java/com/example/allandoamaralalves/upartyproject/MapaEventos.java
+++ app/src/main/java/com/example/allandoamaralalves/upartyproject/MapaEventos.java
@@ -8,14 +8,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Eventos extends FragmentActivity {
+public class MapaEventos extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_eventos);
+        setContentView(R.layout.activity_mapa_eventos);
         setUpMapIfNeeded();
     }
 
