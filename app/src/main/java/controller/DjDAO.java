b4a458f3d5135abceb4d531764ccd9cab77b6a4e
Allan Alves <allandoamaral@gmail.com>
2015-10-05 11:09:42 -0300

    Solicitar Musica

---
+++ app/src/main/java/controller/DjDAO.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import model.JSONParser;
+
+/**
+ * Created by allandoamaralalves on 05/10/15.
+ */
+public class DjDAO {
+    private JSONParser jsonParser = new JSONParser();
+    private JSONObject json;
+
+    private JSONArray djJson = null;
+}
--- app/src/main/java/view/TelaSolicitarMusica.java
+++ app/src/main/java/view/TelaSolicitarMusica.java
@@ -1,20 +1,50 @@
 package view;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.allandoamaralalves.upartyproject.R;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import model.JSONParser;
 
 public class TelaSolicitarMusica extends AppCompatActivity {
 
     private String eventoId, usuarioId;
+    private ProgressDialog pDialog;
 
     private static final String TAG_EVENTO_ID = "evento_id";
+    JSONParser jsonParser = new JSONParser();
+
+    private static String url_get_djs = "http://uparty.3eeweb.com/db_retornar_djs.php";
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_DJS = "djs";
+    // djs JSONArray
+    private JSONArray djs = null;
+
+    private final List<String> listDjs = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +60,8 @@ public class TelaSolicitarMusica extends AppCompatActivity {
 
         TextView txtInfo = (TextView) findViewById(R.id.text_info);
         txtInfo.setText("Info: ID " + usuarioId + " / Evento " + eventoId);
+
+        new LoadDjs().execute();
     }
 
     @Override
@@ -52,5 +84,65 @@ public class TelaSolicitarMusica extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class LoadDjs extends AsyncTask<String, String, String> {
+        private JSONObject json;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(TelaSolicitarMusica.this);
+            pDialog.setMessage("Loading djs. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair(TAG_EVENTO_ID, eventoId));
+            // getting JSON string from URL
+            json = jsonParser.makeHttpRequest(url_get_djs, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Djs: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // retornando json array de eventos
+                    djs = json.getJSONArray(TAG_DJS);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String file_url) {
+            pDialog.dismiss();
+            // percorrendo todos os eventos
+            if (djs != null) {
+                for (int i = 0; i < djs.length(); i++) {
+                    JSONObject c = null;
+                    try {
+                        c = djs.getJSONObject(i);
+                        listDjs.add(c.getString("usuariodj_id"));
+                        System.out.println("LALA DJ - " + c.getString("usuariodj_id"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Spinner sp=(Spinner) findViewById(R.id.spinner_dj);
+                ArrayAdapter<String> adapter;
+                ArrayAdapter<String> adp= new ArrayAdapter<String> (TelaSolicitarMusica.this,
+                        android.R.layout.simple_list_item_1,listDjs);
+                adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                sp.setAdapter(adp);
+            }
+        }
     }
 }
